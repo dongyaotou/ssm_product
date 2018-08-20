054e1f4b7625a83592bdfbee9a0e74ec5a923ef6
zhaoyongqiang <1632484771@qq.com>
2018-08-20 20:38:27 +0800

    第二次提交到本地仓库

--- src/main/java/cn/nb/bean/Product.java
+++ src/main/java/cn/nb/bean/Product.java
@@ -5,7 +5,7 @@ import java.io.Serializable;
 public class Product implements Serializable {
 
 
-    private int id;
+    private int id;//这是商品的id
     private String productName;
     private int quantity;
 
